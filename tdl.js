b32ef5384a09ea6cfbab22bdd2776a1d32f80711
positiveko <69200669+positiveko@users.noreply.github.com>
2020-09-05 18:38:03 +0900

    first To Do List page

---
+++ tdl.js
@@ -0,0 +1,85 @@
+// create a "close" button and append it to each list item
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i =0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("X");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+// add a "checked" symbol when clicking on a list item
+var list = document.querySelector('ul');
+list.addEventListener('click',function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+
+
+// creat a new list item when clicking on the "add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!"); // 스페이스바 적용
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value ="";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("X");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
+
+// creat a new list item when pressing Enter Key
+function addNewItem(list, itemText){
+  var li = document.createElement('li');
+  li.innerText = itemText;
+  list.appendChild(li);
+}
+
+var inputText = document.getElementById('myInput');
+inputText.focus( );
+
+inputText.onkeyup = function(event){
+  if(event.which ===13){
+    var itemText = myInput.value;
+    if(!itemText || itemText === "" || itemText === " ") return false; // blank 방지
+    addNewItem(document.getElementById('myUL'), itemText);
+    inputText.focus();
+    inputText.select();
+    // add "X" mark
+    var li = document.createElement("li");
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("X");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+  }
+};
