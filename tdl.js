f735b2f40e50382242d58b7f008960bb4a75c38f
positiveko <69200669+positiveko@users.noreply.github.com>
2020-09-06 22:14:56 +0900

    엔터 키로 항목 추가 시 X 버튼 뜨지 않는 오류 해결

--- tdl.js
+++ tdl.js
@@ -1,7 +1,7 @@
 // create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
-for (i =0; i < myNodelist.length; i++) {
+for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("X");
   span.className = "close";
@@ -13,21 +13,23 @@ for (i =0; i < myNodelist.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
-  }
+  };
 }
 
 // add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click',function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-
+var list = document.querySelector("ul");
+list.addEventListener(
+  "click",
+  function (ev) {
+    if (ev.target.tagName === "LI") {
+      ev.target.classList.toggle("checked");
+    }
+  },
+  false
+);
 
 // creat a new list item when clicking on the "add" button
 function newElement() {
@@ -35,12 +37,13 @@ function newElement() {
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue === '') {
+  if (inputValue.trim() === "") {
+    // 스페이스, 탭과 같은 공백 문자 지워줌
     alert("You must write something!"); // 스페이스바 적용
   } else {
     document.getElementById("myUL").appendChild(li);
   }
-  document.getElementById("myInput").value ="";
+  document.getElementById("myInput").value = "";
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("X");
@@ -49,28 +52,30 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
-    }
+    };
   }
 }
 
 // creat a new list item when pressing Enter Key
-function addNewItem(list, itemText){
-  var li = document.createElement('li');
+function addNewItem(list, itemText) {
+  var li = document.createElement("li");
   li.innerText = itemText;
   list.appendChild(li);
 }
 
-var inputText = document.getElementById('myInput');
-inputText.focus( );
+var inputText = document.getElementById("myInput");
+inputText.focus();
 
-inputText.onkeyup = function(event){
-  if(event.which ===13){
-    var itemText = myInput.value;
-    if(!itemText || itemText === "" || itemText === " ") return false; // blank 방지
-    addNewItem(document.getElementById('myUL'), itemText);
+inputText.onkeyup = function (event) {
+  if (event.which === 13) {
+    newElement(); // 엔터 키로 항목 추가 시 X버튼 안 뜨는 오류 해결
+    /*
+    var itemText = document.getElementById("myInput").value;
+    if (!itemText || itemText === "" || itemText === " ") return false; // blank 방지
+    addNewItem(document.getElementById("myUL"), itemText);
     inputText.focus();
     inputText.select();
     // add "X" mark
@@ -80,6 +85,6 @@ inputText.onkeyup = function(event){
     span.className = "close";
     span.appendChild(txt);
     li.appendChild(span);
-
+    */
   }
 };
